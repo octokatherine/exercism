a827b579e8540955b1d87689fa6e6a3bfb1d1bef
Katherine Peterson <octokatherine@github.com>
2021-12-01 12:02:43 -0800

    partial bowling solution

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -5,3 +5,51 @@ Write your code for the 'Bowling' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/bowling` directory.
 =end
+
+class Game 
+  class BowlingError < StandardError; end 
+
+  def initialize 
+    @rolls = []
+    @frames = []
+  end
+
+  def roll(pins)
+    raise BowlingError if pins < 0 || pins > 10
+    @rolls << pins 
+  end
+
+  def score 
+    until @frames.length == 10 do 
+      frame = Frame.new(@rolls)
+      @frames << frame 
+      @rolls = frame.process 
+    end
+
+    @frames.collect(&:score).sum 
+  end
+end
+
+class Frame  
+  attr_reader :score 
+
+  def initialize(rolls)
+    @rolls = rolls 
+  end
+
+  def process  
+    if @rolls.first == 10 
+      raise Game::BowlingError if !(@rolls[1] && @rolls[2])
+      @score = 10 + @rolls[1] + @rolls[2] 
+      @rolls.drop(1)
+    elsif @rolls.first + @rolls[1] == 10 
+      @score = 10 + @rolls[2] 
+      @rolls.drop(2)
+    elsif @rolls.first + @rolls[1] > 10
+      raise Game::BowlingError 
+    else
+      @score = @rolls.first + @rolls[1] 
+      @rolls.drop(2)
+    end
+  end
+end
